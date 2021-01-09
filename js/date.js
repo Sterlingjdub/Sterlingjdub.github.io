16f357d35fe6204dfe5ea5b34942f26aabf976db
Sterlingjdub <sterlingjdub@gmail.com>
2021-01-09 15:42:05 -0700

    Updated...again

--- js/date.js
+++ js/date.js
@@ -2,4 +2,4 @@ var x = document.lastModified;
 document.getElementById("lastUpdated").outerHTML = x
 
 new Date().getFullYear()
-document.getElementById("year").outerHTML = new Date().getFullYear();
+document.getElementById("year").outerHTML = new Date().getFullYear(); 
