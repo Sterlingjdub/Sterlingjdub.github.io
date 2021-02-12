142fc776142e59df88811ca5aa061c5eb3824c0f
Sterlingjdub <sterlingjdub@gmail.com>
2021-02-11 23:58:41 -0700

    updated the windchill

--- assignments/lesson-6/js/windchill.js
+++ assignments/lesson-6/js/windchill.js
@@ -4,10 +4,16 @@ var s = parseFloat(document.getElementById('wind').textContent);
 var t = (h + l) / 2;
 
 function windChill(tempF, speed) {
-var f = 35.74 + .6215 * tempF - 35.75 * Math.pow(speed, .16) +
-.4275 * tempF * Math.pow(speed, .16);
-return Math.round(f);
+    if (tempF <= 50 && speed >= 3){
+        var f = 35.74 + .6215 * tempF - 35.75 * Math.pow(speed, .16) +
+        .4275 * tempF * Math.pow(speed, .16);
+        return Math.round(f);
+    }
+    else {
+        return "N/A"
+    }
 }
+
 
 var ans = windChill(t, s);
 document.getElementById('windChill').innerHTML = ans;
