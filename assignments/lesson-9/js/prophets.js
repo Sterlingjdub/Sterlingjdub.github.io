0742c4578082c54f6f96263ee556990157f8b7da
Sterlingjdub <sterlingjdub@gmail.com>
2021-03-04 23:48:41 -0700

    push

--- assignments/lesson-9/js/prophets.js
+++ assignments/lesson-9/js/prophets.js
@@ -5,14 +5,15 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
 
     for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
+
         let p = document.createElement('p');
-        let 
+
 
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
