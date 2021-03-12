e5adcf3dcc61f531b79c4d70d0f353fe5fe87f23
Sterlingjdub <sterlingjdub@gmail.com>
2021-03-12 12:29:18 -0700

    bonus

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -4,9 +4,10 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
+        
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  
         const desc = jsObject.weather[0].description;  
-        
+
         document.getElementById("current-temp").textContent = jsObject.main.temp;
         document.getElementById('imagesrc').textContent = imagesrc;  
         document.getElementById('icon').setAttribute('src', imagesrc);  
