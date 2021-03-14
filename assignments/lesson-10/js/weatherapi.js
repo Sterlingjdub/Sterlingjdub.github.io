3fe266e79a8e571a304faaeecf161ae555ea83a6
Sterlingjdub <sterlingjdub@gmail.com>
2021-03-13 17:56:26 -0700

    push

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -5,9 +5,6 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         //console.log(jsObject);
-        
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  
-        const desc = jsObject.weather[0].description;  
         const temp = Math.round(jsObject.main.temp);
         const speed = Math.round(jsObject.wind.speed);
 
@@ -28,31 +25,34 @@ fetch(apiURL)
         document.getElementById('wind').textContent = Math.round(jsObject.wind.speed);
         document.getElementById('windChill').innerHTML = windChill;
 
-
-
-        document.getElementById('imagesrc').textContent = imagesrc;  
-        document.getElementById('icon').setAttribute('src', imagesrc);  
-        document.getElementById('icon').setAttribute('alt', desc);
-
   });
 
-  fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-      //console.log(jsObject);
-      
-      const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  
-      const desc = jsObject.weather[0].description;  
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
 
-      // This will display the current weather information
-      document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
-      document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
-      document.getElementById('low').textContent = Math.round(jsObject.main.temp_min);
-      document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
-      document.getElementById('wind').textContent = Math.round(jsObject.wind.speed);
+        let count = 0;
+        const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        
+        for (let i = 0; count < 5; i++) {
+          let obj = jsObject.list[i];
+          if (obj.dt_txt.includes("18:00:00")) {
+            count++;
+        
+            //Calculate day of week
+            let date = new Date(obj.dt_txt);
+            let day = date.getDay();
+        
 
-      document.getElementById('imagesrc').textContent = imagesrc;  
-      document.getElementById('icon').setAttribute('src', imagesrc);  
-      document.getElementById('icon').setAttribute('alt', desc);
+            let imagesrc = `https://openweathermap.org/img/w/${obj.weather[0].icon}.png`;
+            let desc = obj.weather[0].description;
+        
+            document.getElementById(`day${count}`).textContent = dayOfWeek[day];
+            document.getElementById(`tempH${count}`).textContent = Math.round(obj.main.temp);
+            document.getElementById(`tempL${count}`).textContent = Math.round(obj.main.temp_min);
+            document.getElementById(`img${count}`).setAttribute('src', imagesrc);
+            document.getElementById(`img${count}`).setAttribute('alt', desc);
+          }
+        }
 
 });
