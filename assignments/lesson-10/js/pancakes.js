6ff44d36a9103fd7b61bcd892ad7a53d61b6557e
Sterlingjdub <sterlingjdub@gmail.com>
2021-03-13 19:33:17 -0700

    update

--- assignments/lesson-10/js/pancakes.js
+++ assignments/lesson-10/js/pancakes.js
@@ -1,5 +1,6 @@
 // This will display the weekly banner only on Fridays
-let day = new Date().getDay();
+let d = new Date();
+let day = d.getDay();
 if (day == 5) {
     document.getElementById("banner").style.display = "block";
 }
