5ed4e854e22bbc0fb5256215a098059a1807c7ea
Sterlingjdub <sterlingjdub@gmail.com>
2021-03-19 17:09:23 -0600

    favicon

--- assignments/lesson-11/js/pancakes.js
+++ assignments/lesson-11/js/pancakes.js
@@ -1,9 +1,11 @@
 // This will display the weekly banner only on Fridays
 let d = new Date();
 let day = d.getDay();
-if (day == 5) {
-    document.getElementById("banner").style.display = "block";
-}
-else {
-    document.getElementById("banner").style.display = "none";
+{
+    if (day == 5) {
+        document.getElementById("banner").style.display = "block";
+    }
+    else {
+        document.getElementById("banner").style.display = "none";
+    }
 }
