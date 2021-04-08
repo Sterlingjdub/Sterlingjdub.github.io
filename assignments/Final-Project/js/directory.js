aca5249f3b6ef7a0e12743dd4272a7532d1ce53f
Sterlingjdub <sterlingjdub@gmail.com>
2021-04-08 13:34:19 -0600

    directory

--- assignments/Final-Project/js/directory.js
+++ assignments/Final-Project/js/directory.js
@@ -1,34 +1,33 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-fetch(requestURL)
+fetch("directory.json")
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
-        //console.table(jsonObject);  // temporary checking for valid response and data parsing
-        const prophets = jsonObject['prophets'];
-
-        for (let i = 0; i < prophets.length; i++ ) {
+        const businesses = jsonObject['business'];
+        for (let i = 0; i < businesses.length; i++ ) {
+            //Creating elements
             let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-
+            let h5 = document.createElement('h5');
             let p = document.createElement('p');
-            let birthDay = document.createElement('p');
-            let birthPlace = document.createElement('p');
+            let a = document.createElement('a');
+            let number = document.createElement('p');
             let image = document.createElement('img');
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            birthDay.textContent = "Date of Birth: " + prophets[i].birthdate;
-            birthPlace.textContent = "Place of Birth: " + prophets[i].birthplace
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name + prophets[i].lastname + ' ' + prophets[i].order);
+            h5.textContent = businesses[i].name;
+            number.textContent = businesses[i].phone;
+            a.textContent = businesses[i].name + " Website";
+            a.setAttribute("href", businesses[i].link);
+            image.setAttribute('src', businesses[i].image);
+            image.setAttribute('alt', businesses[i].name + " logo");
 
-            card.appendChild(h2);
+            //Appending the elements
+            card.appendChild(h5);
             card.appendChild(p);
-            p.appendChild(birthDay);
-            p.appendChild(birthPlace);
+            p.appendChild(number);
+            p.appendChild(a);
             card.appendChild(image);
 
             document.querySelector('div.cards').appendChild(card);
-    }
-  });
+        }
+    });
