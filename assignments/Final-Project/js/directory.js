811b6ce759c0fe1fd4bea7f7627222944648b98b
Sterlingjdub <sterlingjdub@gmail.com>
2021-04-08 14:51:46 -0600

    emails

--- assignments/Final-Project/js/directory.js
+++ assignments/Final-Project/js/directory.js
@@ -12,10 +12,12 @@ fetch("directory.json")
             let p = document.createElement('p');
             let a = document.createElement('a');
             let number = document.createElement('p');
+            let email = document.createElement('p');
             let image = document.createElement('img');
 
             h5.textContent = businesses[i].name;
             number.textContent = businesses[i].phone;
+            email.textContent = businesses[i].email;
             a.textContent = businesses[i].name + " Website";
             a.setAttribute("href", businesses[i].link);
             image.setAttribute('src', businesses[i].image);
@@ -25,9 +27,22 @@ fetch("directory.json")
             card.appendChild(h5);
             card.appendChild(p);
             p.appendChild(number);
+            p.appendChild(email);
             p.appendChild(a);
             card.appendChild(image);
 
             document.querySelector('div.cards').appendChild(card);
         }
     });
+
+    //Display grid
+function displayGrid() {
+    document.getElementById("cards").classList.add("grid");
+    document.getElementById("cards").classList.remove("list");
+}
+  
+  //Display list
+function displayList() {
+    document.getElementById("cards").classList.add("list");
+    document.getElementById("cards").classList.remove("grid");
+}
